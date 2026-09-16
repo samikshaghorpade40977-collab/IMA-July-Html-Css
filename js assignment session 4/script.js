d405ea16060133daf9c7068bd521e445fa06d9a5
samikshaghorpade40977-collab <samikshaghorpade40977@gmail.com>
2026-09-16 22:23:06 +0530

    Create script.js

---
+++ js assignment session 4/script.js
@@ -0,0 +1,20 @@
+let age = "25";
+let salary = "50000";
+let experience = "2.5";
+
+age = Number(age);
+salary = Number(salary);
+experience = Number(experience);
+
+console.log("Age:", age);
+console.log("Type of Age:", typeof age);
+
+console.log("Salary:", salary);
+console.log("Type of Salary:", typeof salary);
+
+console.log("Experience:", experience);
+console.log("Type of Experience:", typeof experience);
+
+let yearlySalary = salary * 12;
+
+console.log("Yearly Salary:", yearlySalary);
